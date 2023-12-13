5a28c0db08dfdf77fa4e5bc87c934f768d40c990
anagabrieleloor <anagabrieleloor@outlook.com>
2023-12-13 13:09:50 -0500

    api string

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:8088/api';
+// const BASE_URL = 'http://localhost:8088/api';
+const BASE_URL = 'https://carino.onrender.com';
 
 export async function fetchUsers() {
     try {
