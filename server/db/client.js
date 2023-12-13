05598b6ea3ddfbfbd0c6ac433bc2ce32db2a4f30
anagabrieleloor <anagabrieleloor@outlook.com>
2023-12-13 12:58:19 -0500

    deploy db connection string

--- server/db/client.js
+++ server/db/client.js
@@ -3,7 +3,9 @@ const { Client } = require('pg')
 
 //establish connection to database
 const carino = 'carino'
-const client = new Client(`postgres://localhost:5432/${carino}`)
+// const client = new Client(`postgres://localhost:5432/${carino}`)
+
+const client = new Client((`postgres://carino_user:NMpH6AwHq0rzkqBiV0zdUcxEQKQo9tiH@dpg-clsuvripmc4c73dtfn5g-a/carino`));
 
 
 //esport for use in other files
